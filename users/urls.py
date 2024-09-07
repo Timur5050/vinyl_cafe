c94518e1f4e74258549a8dc52eb45473c55259dd
timur <stukantimur811@gmail.com>
2024-09-07 17:08:26 +0300

    Added jwt token support in urls

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,11 @@
-from django.urls import path, include
+from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 from users.views import CreateUser
 
 urlpatterns = [
     path("registration/", CreateUser.as_view(), name="register_user"),
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
