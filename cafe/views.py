17964d3bd99e0e3ec264fd748f4db6b60c92118a
timur <stukantimur811@gmail.com>
2024-09-07 18:03:26 +0300

    created model view set for all actions for product

--- cafe/views.py
+++ cafe/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from cafe.models import Product
+from cafe.permissions import IsAdminOrReadOnly
+from cafe.serializers import (
+    ProductRetrieveCreateSerializer,
+    ProductListSerializer
+)
+
+
+class ProductView(ModelViewSet):
+    serializer_class = ProductRetrieveCreateSerializer
+    queryset = Product.objects.all()
+    permission_classes = [IsAdminOrReadOnly]
+
+    def get_serializer_class(self):
+        serializer = ProductRetrieveCreateSerializer
+        if self.action == "list":
+            serializer = ProductListSerializer
+        return serializer
