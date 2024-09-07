3b40754b26e8755edb46791c1c0716f86d65ce93
timur <stukantimur811@gmail.com>
2024-09-07 19:34:38 +0300

    added endpoint for list and creating of orders

--- cafe/urls.py
+++ cafe/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from cafe.views import ProductView
+from cafe.views import ProductView, OrderView
 
 router = routers.DefaultRouter()
 router.register("products", ProductView)
 
 urlpatterns = [
     path("", include(router.urls)),
+    path("orders/", OrderView.as_view(), name="orders"),
 ]
