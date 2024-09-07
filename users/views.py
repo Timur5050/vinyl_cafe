40901b01bd149a98267e2eee2268e676bca03882
timur <stukantimur811@gmail.com>
2024-09-07 16:52:23 +0300

    added users registration

--- users/views.py
+++ users/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework.generics import CreateAPIView
 
-# Create your views here.
+from users.serializers import UserCreateSerializer
+
+
+class CreateUser(CreateAPIView):
+    serializer_class = UserCreateSerializer
+    queryset = get_user_model().objects.all()
