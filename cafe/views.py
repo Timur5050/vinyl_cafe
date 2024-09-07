9b87b7d3736d9952aeb62455177a8df6d5c474e3
timur <stukantimur811@gmail.com>
2024-09-07 19:32:44 +0300

    created view for creating and getting list of orders

--- cafe/views.py
+++ cafe/views.py
@@ -1,10 +1,12 @@
+from rest_framework.generics import ListCreateAPIView
 from rest_framework.viewsets import ModelViewSet
 
-from cafe.models import Product
+from cafe.models import Product, Order
 from cafe.permissions import IsAdminOrReadOnly
 from cafe.serializers import (
     ProductRetrieveCreateSerializer,
-    ProductListSerializer
+    ProductListSerializer,
+    OrderListRetrieveSerializer, OrderCreateSerializer
 )
 
 
@@ -18,3 +20,21 @@ class ProductView(ModelViewSet):
         if self.action == "list":
             serializer = ProductListSerializer
         return serializer
+
+
+class OrderView(ListCreateAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderListRetrieveSerializer
+    permission_classes = [IsAdminOrReadOnly]
+
+    def get_serializer_class(self):
+        serializer = OrderListRetrieveSerializer
+        if self.request.method == "POST":
+            serializer = OrderCreateSerializer
+
+        return serializer
+
+    def get_queryset(self):
+        queryset = Order.objects.filter(user=self.request.user)
+        return queryset
+
