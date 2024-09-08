3ca003ccd881525a6254924b78391b299411a3db
timur <stukantimur811@gmail.com>
2024-09-08 20:10:50 +0300

    added feedback urls

--- cafe/urls.py
+++ cafe/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from cafe.views import ProductView, OrderView
+from cafe.views import ProductView, OrderView, FeedBackView
 
 router = routers.DefaultRouter()
 router.register("products", ProductView)
@@ -9,4 +9,6 @@ router.register("products", ProductView)
 urlpatterns = [
     path("", include(router.urls)),
     path("orders/", OrderView.as_view(), name="orders"),
+    path("feedback/", FeedBackView.as_view(), name="feedback"),
+    path("feedback/<int:pk>/", FeedBackView.as_view(), name="feedback"),
 ]
